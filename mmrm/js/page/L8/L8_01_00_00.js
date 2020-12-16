5d521d45774277187081ada18552c36e7b8f82f2
harvey <harvey@henghung.com>
2020-12-16 15:27:31 +0800

    update

--- mmrm/js/page/L8/L8_01_00_00.js
+++ mmrm/js/page/L8/L8_01_00_00.js
@@ -52,6 +52,7 @@ export default function({ apiUrl, pageUrl }) {
             let hyperlink_url = linkObj.hyperlink_url;
             this.isLoading = false;
             location.href = hyperlink_url;
+            alert('test')
             // if (!this.checkIsHttps(hyperlink_url)) liff.closeWindow();
             return;
          }

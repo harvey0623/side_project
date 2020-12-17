2347789856bf5a04ec7b37c6e6aa471dae1afe33
harvey <harvey@henghung.com>
2020-12-17 10:59:34 +0800

    update

--- mmrm/js/page/L8/L8_01_00_01.js
+++ mmrm/js/page/L8/L8_01_00_01.js
@@ -38,7 +38,7 @@ export default function({ apiUrl }) {
          this.isLoading = true;
          let pageId = this.getQuery('page_id');
          this.pageData = await this.getFaqPage(pageId).then(res => res);
-         document.querySelector('title').textContent = this.pageTitle;
+         // document.querySelector('title').textContent = this.pageTitle;
          this.isLoading = false;
       }
    });

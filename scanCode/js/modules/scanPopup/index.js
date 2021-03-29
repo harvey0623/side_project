5116ed1a1686792dadd2062eff2e6c2ab1934744
harvey <harvey@henghung.com>
2021-03-29 10:59:31 +0800

    update qrscanner

---
+++ scanCode/js/modules/scanPopup/index.js
@@ -0,0 +1,30 @@
+import QrScanner from "../../../lib/qr-scanner.min.js";
+QrScanner.WORKER_PATH = '../../../lib/qr-scanner-worker.min.js';
+export default class ScanPopup {
+   constructor(props) {
+      this.el = document.querySelector(props.el);
+      this.camera = this.el.querySelector('#qr-video');
+      this.scanner = null;
+      this.isFirstOpen = true;
+      this.scanEvent = props.scanEvent;
+   }
+   async display(isOpen) {
+      this.el.style.display = isOpen ? 'block' : 'none';
+      if (this.isFirstOpen) {
+         this.isFirstOpen = false;
+         let hasCamera = await QrScanner.hasCamera();
+         if (hasCamera) this.initScanner();
+      }
+      if (!isOpen) {
+         if (this.scanner !== null) this.scanner.stop();
+      }
+   }
+   initScanner() {
+      this.scanner = new QrScanner(this.camera, (code) => {
+         this.scanEvent(code);
+         this.scanner.stop();
+      });
+      this.scanner.setInversionMode('both');
+      this.scanner.start();
+   }
+}

2d5d6be23ec05a983edaf40ccf8f9362046963b5
harvey <harvey@henghung.com>
2021-03-29 14:51:44 +0800

    update

--- scanCode/js/modules/scanPopup/index.js
+++ scanCode/js/modules/scanPopup/index.js
@@ -1,11 +1,13 @@
-import QrScanner from "../../../lib/qr-scanner.min.js";
-QrScanner.WORKER_PATH = '../../../lib/qr-scanner-worker.min.js';
 export default class ScanPopup {
    constructor(props) {
       this.el = document.querySelector(props.el);
-      this.camera = this.el.querySelector('#qr-video');
+      this.canvas = this.el.querySelector('#canvas');
+      this.ctx = this.canvas.getContext('2d');
+      this.video = this.el.querySelector('#video');
       this.closeItem = this.el.querySelector('.closeItem');
       this.scanner = null;
+		this.animateFrame = null;
+		this.isScan = false;
       this.isFirstOpen = true;
       this.scanEvent = props.scanEvent;
       this.bindEvent();
@@ -17,31 +19,62 @@ export default class ScanPopup {
    }
    async display(isOpen) {
       this.el.style.display = isOpen ? 'block' : 'none';
-      if (this.isFirstOpen) {
-         this.isFirstOpen = false;
-         // let hasCamera = await QrScanner.hasCamera();
-         QrScanner.hasCamera().then(hasCamera => {
-            alert(hasCamera);
-            if (hasCamera) this.initScanner();
-         })
-         // if (hasCamera) this.initScanner();
-         return;
-      }
-      let hasScanner = this.scanner !== null;
       if (isOpen) {
-         if (hasScanner) this.scanner.start();
+         this.getMedia();
       } else {
-         if (hasScanner) this.scanner.stop();
+         cancelAnimationFrame(this.animateFrame);
+         this.video.pause();
+         this.isScan = false;
       }
    }
-   initScanner() {
-      alert('start')
-      this.scanner = new QrScanner(this.camera, function() {
-         alert(result);
-         //this.scanEvent(code);
-         // this.scanner.stop();
-      });
-      this.scanner.start();
-      this.scanner.setInversionMode('both');
+   drawLine(begin, end) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(begin.x, begin.y);
+      this.ctx.lineTo(end.x, end.y);
+      this.ctx.lineWidth = 2;
+      this.ctx.strokeStyle = '#FF3B58';
+      this.ctx.stroke();
+   }
+   tick() {
+      if (this.video.readyState === this.video.HAVE_ENOUGH_DATA) {
+         let size = { w: 300, h: 300 };
+         this.canvas.classList.remove('notActive');
+         this.ctx.drawImage(this.video, 0, 0, size.w, size.h);
+         let { data, width, height } = this.ctx.getImageData(0, 0, size.w, size.h);
+         let code = jsQR(data, width, height, {
+            inversionAttempts: 'dontInvert',
+         });
+         if (code) {
+            this.isScan = true;
+            let { topLeftCorner, topRightCorner, bottomRightCorner, bottomLeftCorner } = code.location;
+            this.drawLine(topLeftCorner, topRightCorner);
+            this.drawLine(topRightCorner, bottomRightCorner);
+            this.drawLine(bottomRightCorner, bottomLeftCorner);
+            this.drawLine(bottomLeftCorner, topLeftCorner);
+            alert(code.data);
+            this.video.pause();
+            cancelAnimationFrame(this.animateFrame);
+            this.scanEvent(code.data);
+         }
+      }
+      if (!this.isScan) {
+         this.animateFrame = requestAnimationFrame(() => {
+            this.tick();
+         });
+      }
+   }
+   async getMedia() {
+      try {
+         let stream = await navigator.mediaDevices.getUserMedia({ 
+            video: { facingMode: 'environment' }
+         });
+         this.video.srcObject = stream;
+         this.video.play();
+         this.animateFrame = requestAnimationFrame(() => {
+            this.tick();
+         });
+      } catch(err) {
+         alert('您未開起相機功能')
+      }
    }
 }

b0bab53dfd22cd6e8ee93c8b8f9b56ca8206d7eb
harvey <harvey@henghung.com>
2021-03-29 15:37:57 +0800

    update

--- scanCode/js/modules/scanPopup/index.js
+++ scanCode/js/modules/scanPopup/index.js
@@ -5,8 +5,8 @@ export default class ScanPopup {
       this.ctx = this.canvas.getContext('2d');
       this.video = this.el.querySelector('#video');
       this.closeItem = this.el.querySelector('.closeItem');
-		this.animateFrame = null;
-		this.isScan = false;
+      this.animateFrame = null;
+      this.isScan = false;
       this.isFirstOpen = true;
       this.scanEvent = props.scanEvent;
       this.bindEvent();
@@ -37,12 +37,12 @@ export default class ScanPopup {
    tick() {
       if (this.video.readyState === this.video.HAVE_ENOUGH_DATA) {
          let size = { w: 300, h: 300 };
+         this.canvas.classList.remove('notActive');
          this.ctx.drawImage(this.video, 0, 0, size.w, size.h);
          let { data, width, height } = this.ctx.getImageData(0, 0, size.w, size.h);
          let code = jsQR(data, width, height, {
             inversionAttempts: 'dontInvert',
          });
-         console.log(code);
          if (code) {
             this.isScan = true;
             let { topLeftCorner, topRightCorner, bottomRightCorner, bottomLeftCorner } = code.location;
@@ -50,21 +50,22 @@ export default class ScanPopup {
             this.drawLine(topRightCorner, bottomRightCorner);
             this.drawLine(bottomRightCorner, bottomLeftCorner);
             this.drawLine(bottomLeftCorner, topLeftCorner);
-            this.display(false);
-            cancelAnimationFrame(this.animateFrame);
+            console.log(code.data);
+            alert(code.data);
             this.video.pause();
-            this.scanEvent(code.data);
+            cancelAnimationFrame(this.animateFrame);
+            this.scanEvent();
          }
-         if (!this.isScan) {
-            this.animateFrame = requestAnimationFrame(() => {
-               this.tick();
-            });
-         }
+      }
+      if (!this.isScan) {
+         this.animateFrame = requestAnimationFrame(() => {
+            this.tick();
+         });
       }
    }
    async getMedia() {
       try {
-         let stream = await navigator.mediaDevices.getUserMedia({ 
+         let stream = await navigator.mediaDevices.getUserMedia({
             video: { facingMode: 'environment' }
          });
          this.video.srcObject = stream;
@@ -72,7 +73,7 @@ export default class ScanPopup {
          this.animateFrame = requestAnimationFrame(() => {
             this.tick();
          });
-      } catch(err) {
+      } catch (err) {
          alert('您未開起相機功能')
       }
    }

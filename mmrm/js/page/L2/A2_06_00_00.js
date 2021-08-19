1c9368249879f9decd9db77454da8882b513dc99
harvey <harvey@henghung.com>
2021-08-19 17:26:40 +0800

    update

--- mmrm/js/page/L2/A2_06_00_00.js
+++ mmrm/js/page/L2/A2_06_00_00.js
@@ -31,7 +31,7 @@ export default function ({ apiUrl, pageUrl }) {
          shareHandler() {
             this.checkHandler();
             if (this.isEmpty) return;
-            let text = `Hi ! 好朋友 \n下載王品瘋美食APP\n[wwww.wowfoods.cc/mgm]\n輸入我的好友推薦碼\n[ ${this.user.code} ]，跟我一起享受美食吧!`;
+            let text = `Hi 好朋友\n下載王品瘋美食APP\nhttps://wowfoods.cc/mgm\n首次註冊輸入我的好友推薦碼﹝${this.profile.referral_code}﹞，一起享受美食吧!`;
             let url = `line://msg/text/${encodeURIComponent(text)}`;
             location.href = url;
          }

ae08dbabce6601de0712232a8ae65ba4ca240d09
harvey <harvey@henghung.com>
2021-08-19 17:29:35 +0800

    update

--- mmrm/js/src/vue-mixin/localProfile.js
+++ mmrm/js/src/vue-mixin/localProfile.js
@@ -3,7 +3,8 @@ window.localProfile = {
       return {
          profile: {
             name: '',
-            avatar: ''
+            avatar: '',
+            referral_code: ''
          }
       }
    },
@@ -18,11 +19,29 @@ window.localProfile = {
          let storage = localStorage.getItem('member_profile');
          let profile = storage !== null ? JSON.parse(storage) : null;
          this.profile.name = profile !== null ? profile.name : '';
+         this.profile.referral_code = profile !== null ? profile.referral_code : '';
          if (profile === null || profile.photo === undefined) {
             this.profile.avatar = '';
          } else {
             this.profile.avatar = profile.photo.url;
          }
       },
+      getMemberProfile() {
+         let meta = document.querySelector('[name="get_member_profile_url"]');
+         if (meta === null) return;
+         return mmrmAxios({
+            url: meta.content,
+            method: 'post',
+            data: {}
+         }).then(res => res.data.results.member_profile).catch(() => null);
+      },
+   },
+   mounted() {
+      let storage = localStorage.getItem('member_profile');
+      if (storage !== null) return;
+      this.getMemberProfile().then(res => {
+         localStorage.setItem('member_profile', JSON.stringify(res));
+         this.getLocalProfile();
+      });
    }
 }
